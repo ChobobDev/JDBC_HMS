ae5c828b4deeb7ce4b8f92de9092dda4da4145f2
Seongbin Bernie Cho <32432141+ChobobDev@users.noreply.github.com>
2020-04-30 17:24:36 +0900

    booking check

--- src/com/assignment/application/guest_mode.java
+++ src/com/assignment/application/guest_mode.java
@@ -1,47 +1,87 @@
 package com.assignment.application;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.Date;
 
 
 public class guest_mode extends JFrame {
+    private static String csse = "jdbc:mysql://csse-mysql.xjtlu.edu.cn:3306/SCho18?user=SCho18&password=123";
+
     JTable table;
+    JScrollPane scp;
     JLabel un_label;
     JButton lg_out, book_room;
     JFrame frame = new JFrame("Sunny Isle Hotel");
+    DefaultTableModel model = new DefaultTableModel(new String[]{"Room Number", "Room Type", "Check IN","Check OUT","Food"}, 0);
     public guest_mode(String un){
-        new show_booked(un);
+        table = new JTable(model);
+        scp = new JScrollPane(table);
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(csse);
+            String data = "select * from booked_room where username like'"+un+"';";
+            Statement statement = conn.createStatement();
+            ResultSet res = statement.executeQuery(data);
+            while(res.next()){
+                String rn_text = "";
+                int rn = res.getInt("room_num");
+                rn_text = String.valueOf(rn);
+                int rt = res.getInt("room_type");
+                String rt_text = "";
+                if(rt==1){
+                    rt_text = "1.Large room with double beds";
+                }
+                else if (rt==2){
+                    rt_text = "2.Large room with a large single bed.";
+                }
+                else if (rt==3){
+                    rt_text = "3.Small room with a single bed";
+                }
+                else{
+                    rt_text ="4.VIP room";
+                }
+                Date ci = res.getDate("check_in");
+                String ci_text = String.valueOf(ci);;
+                Date co = res.getDate("check_out");
+                String co_text = String.valueOf(co);
+                int fd = res.getInt("food_status");
+                String fs = "";
+                if(fd==1){
+                    fs="Booked";
+                }
+                else{
+                    fs="None";
+                }
+                model.addRow(new Object[]{rn_text,rt_text,ci_text,co_text,fs});
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        table.setModel(model);
+
         frame.setSize(800,500);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().setLayout(null);
-
-        String data[][]={ {"101","Amit","670000"},
-                {"102","Jai","780000"},
-                {"101","Sachin","700000"}};
-        String column[]={"Room_number","Room Type","Check in","Check out"};
-
         un_label = new JLabel("Username : "+un);
         lg_out = new JButton("Log-out");
         book_room = new JButton("Book a Room");
-        table = new JTable(data,column);
-
 
         un_label.setBounds(610,0,150,30);
         lg_out.setBounds(660,30,100,30);
         book_room.setBounds(30,30,150,30);
-        table.setBounds(30,80,740,300);
+        scp.setBounds(30,100,740,300);
         frame.getContentPane().add(un_label);
         frame.getContentPane().add(lg_out);
         frame.getContentPane().add(book_room);
-        frame.getContentPane().add(table);
+        frame.getContentPane().add(scp);
+        lg_out.addActionListener(e -> {frame.dispose();});
 
 
         frame.setVisible(true);
-
-        lg_out.addActionListener(e -> {frame.dispose();});
 
     }
 }
--- src/com/assignment/application/show_booked.java
+++ src/com/assignment/application/show_booked.java
@@ -1,10 +1,14 @@
 package com.assignment.application;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 
 public class show_booked {
     private static String csse = "jdbc:mysql://csse-mysql.xjtlu.edu.cn:3306/SCho18?user=SCho18&password=123";
+    Object ob[][]=new Object[0][5];
+    DefaultTableModel model;
+
 
     public show_booked(String un){
         Connection conn = null;
@@ -12,8 +16,14 @@ public class show_booked {
             conn = DriverManager.getConnection(csse);
             String booked_result = "select * from booked_room where username like'"+un+"';";
             Statement statement = conn.createStatement();
-            ResultSet reuslt = statement.executeQuery(booked_result);
-            while (reuslt.next()){
+            ResultSet result = statement.executeQuery(booked_result);
+            while (result.next()){
+                String room_num = result.getString("room_num");
+                String room_type = result.getString("room_type");
+                String check_in = result.getString("check_in");
+                String check_out = result.getString("check_out");
+                String food = result.getString("food_status");
+                Object data[]= {room_num,room_type,check_in,check_out,food};
 
             }
         }catch (SQLException e) {
