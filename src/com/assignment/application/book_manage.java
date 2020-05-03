8b4a3def3a977d2519505742901d0908de1393d4
Seongbin Bernie Cho <32432141+ChobobDev@users.noreply.github.com>
2020-05-03 23:06:43 +0900

    MANAGE_FRAME

--- src/com/assignment/application/book_manage.java
+++ src/com/assignment/application/book_manage.java
@@ -1,15 +1,18 @@
 package com.assignment.application;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class book_manage extends JFrame {
     JFrame manageframe;
     JTable table_book;
+    public static DefaultTableModel model = new DefaultTableModel(new String[]{"Select","Room Number", "Check IN"}, 0);
     public book_manage(){
         manageframe = new JFrame("Manage Reservation");
         manageframe.setLocationRelativeTo(null);
         manageframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         manageframe.getContentPane().setLayout(null);
+
 
 
         manageframe.setVisible(true);
