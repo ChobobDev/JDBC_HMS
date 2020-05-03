5a8a04ec43ad75eab4286187176da316206506de
Seongbin Bernie Cho <32432141+ChobobDev@users.noreply.github.com>
2020-05-03 16:58:12 +0900

    MANAGE_FRAME

--- src/com/assignment/application/book_manage.java
+++ src/com/assignment/application/book_manage.java
@@ -3,7 +3,9 @@ package com.assignment.application;
 import javax.swing.*;
 
 public class book_manage extends JFrame {
+    JFrame manageframe;
     public book_manage(){
+        manageframe = new JFrame();
 
     }
 }
