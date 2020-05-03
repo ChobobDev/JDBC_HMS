83b8cbc26fbef2a78d97787d83a829009d9f5cb0
Seongbin Bernie Cho <32432141+ChobobDev@users.noreply.github.com>
2020-05-03 17:17:08 +0900

    MANAGE_FRAME

--- src/com/assignment/application/book_manage.java
+++ src/com/assignment/application/book_manage.java
@@ -6,6 +6,7 @@ public class book_manage extends JFrame {
     JFrame manageframe;
     public book_manage(){
         manageframe = new JFrame("Manage Reservation");
+        manageframe.setLocationRelativeTo(null);
 
     }
 }
